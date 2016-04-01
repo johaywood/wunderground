12491af27927812a7ddbe16e6857e647efe96c1e
Josh Haywood <josh.haywood@validic.com>
2016-04-01 08:00:50 -0400

    allowed chained resource calls

--- spec/wunderground/resources_spec.rb
+++ spec/wunderground/resources_spec.rb
@@ -7,14 +7,28 @@ describe Wunderground::Resources do
         VCR.use_cassette("resource_#{resource}_get_spec") do
           response = Wunderground::Resources.public_send(resource, { zip: '10001' })
           expect(response).to be_a Hash
+          expect(response['response']['features']).to have_key resource.to_s
         end
+      end
+    end
+
+    it 'allows for chaining of methods with _and_' do
+      VCR.use_cassette("resource_geolookup_forecast_get_spec") do
+        response = Wunderground::Resources.geolookup_and_forecast(zip: '10001')
+        expect(response).to be_a Hash
+        expect(response['response']['features']).to have_key 'geolookup'
+        expect(response['response']['features']).to have_key 'forecast'
       end
     end
   end
 
   describe 'non resource methods' do
-    it 'raises a NoMethodError' do
+    it 'raises a NoMethodError for single endpoint' do
       expect { Wunderground::Resources.foo(zip: '10001') }.to raise_error(NoMethodError)
+    end
+
+    it 'raises a NoMethodError if one of multi endpoint call invalid' do
+      expect { Wunderground::Resources.forecast_and_foo(zip: '10001') }.to raise_error(NoMethodError)
     end
   end
 end
